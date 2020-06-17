66e873345e2f3b81bc3f6728647cab808006ddb2
Jesus Terrazas <67078731+ProxyProgrammer@users.noreply.github.com>
2020-06-17 16:34:07 -0700

    Update tcp_logger.py

--- tcp_logger.py
+++ tcp_logger.py
@@ -2,16 +2,16 @@
 
 import socket
 
-HOST, PORT = '', 8888
+HOST, PORT = '', 8888 # # Port and IP that the server Listens On
 
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
-listen_socket.listen(3)
+listen_socket.listen(3) # Starts Listeing for Incoming Connections
 
 
 while True:
-	client_connection, client_address = listen_socket.accept()
+	client_connection, client_address = listen_socket.accept() # Accepts the Connection & gets IP Address
 	print("[!] IP Logged: " + (str(client_address)))
 	request = client_connection.recv(1024)
 	print(request.decode('utf-8'))
